8c84632ff1d1edb603be4a4f97e99282fed3f585
ACHTK <yemengzhidiandig@gmail.com>
2023-10-11 21:55:56 +0800

    1.添加部分数据方言

--- spring-support-generator-starter/src/main/java/com/chua/starter/gen/support/query/TableQuery.java
+++ spring-support-generator-starter/src/main/java/com/chua/starter/gen/support/query/TableQuery.java
@@ -1,5 +1,7 @@
 package com.chua.starter.gen.support.query;
 
+import com.chua.common.support.bean.BeanUtils;
+import com.chua.common.support.session.query.SessionQuery;
 import com.chua.starter.mybatis.entity.RequestPage;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -31,4 +33,9 @@ public class TableQuery extends RequestPage<TableQuery> {
      * 关键字
      */
     private String keyword;
+
+
+    public SessionQuery createSessionQuery() {
+        return BeanUtils.copyProperties(this, SessionQuery.class);
+    }
 }
