af23c8e920d993dbf76424c831b23d5b7086331a
ACHTK <yemengzhidiandig@gmail.com>
2023-10-13 21:04:50 +0800

    fix 1.nginx工具 2.es工具

---
+++ spring-support-generator-starter/src/main/java/com/chua/starter/gen/support/service/impl/SysGenBackupServiceImpl.java
@@ -0,0 +1,15 @@
+package com.chua.starter.gen.support.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chua.starter.gen.support.entity.SysGenBackup;
+import com.chua.starter.gen.support.mapper.SysGenBackupMapper;
+import com.chua.starter.gen.support.service.SysGenBackupService;
+import org.springframework.stereotype.Service;
+/**
+ *    
+ * @author CH
+ */     
+@Service
+public class SysGenBackupServiceImpl extends ServiceImpl<SysGenBackupMapper, SysGenBackup> implements SysGenBackupService{
+
+}
