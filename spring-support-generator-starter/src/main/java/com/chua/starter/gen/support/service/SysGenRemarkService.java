8f612049d6cde36e6e970d9e867c9524e28ff13f
ACHTK <yemengzhidiandig@gmail.com>
2023-10-18 21:38:47 +0800

    1.提供公共接口

---
+++ spring-support-generator-starter/src/main/java/com/chua/starter/gen/support/service/SysGenRemarkService.java
@@ -0,0 +1,8 @@
+package com.chua.starter.gen.support.service;
+
+import com.chua.starter.gen.support.entity.SysGenRemark;
+import com.baomidou.mybatisplus.extension.service.IService;
+public interface SysGenRemarkService extends IService<SysGenRemark>{
+
+
+}
