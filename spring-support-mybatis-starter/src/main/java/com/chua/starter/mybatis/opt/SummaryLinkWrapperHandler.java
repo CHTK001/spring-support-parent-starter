65a453d96ded248494daa5fac45a3a1d6db2ce63
ACHTK <yemengzhidiandig@gmail.com>
2023-11-01 22:25:47 +0800

    1.提供公共接口

--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/opt/SummaryLinkWrapperHandler.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/opt/SummaryLinkWrapperHandler.java
@@ -1,11 +1,13 @@
 package com.chua.starter.mybatis.opt;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.chua.common.support.request.ItemExpression;
 import com.chua.common.support.request.ItemFilter;
 import com.chua.common.support.request.LinkOption;
 import com.chua.common.support.request.OptOption;
 
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * 摘要链接包装处理程序
@@ -17,22 +19,23 @@ public class SummaryLinkWrapperHandler {
     private final String column;
     private final OptOption option;
     private final LinkOption linkOption;
-    private final ItemFilter value;
+    private final ItemFilter itemFilter;
     private final Map<String, String> fields;
+    private final ItemExpression key;
+    private final ItemExpression value;
 
-    public SummaryLinkWrapperHandler(String column, OptOption option, LinkOption linkOption, ItemFilter value, Map<String, String> fields) {
+    public SummaryLinkWrapperHandler(String column, LinkOption linkOption, ItemFilter itemFilter, Map<String, String> fields) {
         this.column = column;
-        this.option = option;
         this.linkOption = linkOption;
-        this.value = value;
+        this.itemFilter = itemFilter;
+        this.key = itemFilter.getKey();
+        this.value = itemFilter.getValue();
+        this.option = itemFilter.getOption();
         this.fields = fields;
     }
 
 
     public <T> void doInject(QueryWrapper<T> t) {
-        if(option == OptOption.EQ) {
-            new SummaryWrapperHandler(value, linkOption, fields).doInject(t);
-            t.eq(column, new )
-        }
+       new SummaryWrapperHandler(itemFilter, linkOption, fields).doInject(t);
     }
 }
