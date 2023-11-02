b8ec6a8564cc74dc2abb13036bc93cf539202f84
ACHTK <yemengzhidiandig@gmail.com>
2023-11-02 16:14:29 +0800

    fix 1.设备接口 2.云接口

--- spring-support-device-starter/src/main/java/com/chua/starter/device/support/request/EventRequest.java
+++ spring-support-device-starter/src/main/java/com/chua/starter/device/support/request/EventRequest.java
@@ -1,9 +1,6 @@
 package com.chua.starter.device.support.request;
 
 import lombok.Data;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Date;
 
 /**
  * 事件请求
@@ -16,6 +13,6 @@ public class EventRequest {
     String filter;
     String deviceDataEventInOrOut;
     EventType eventType;
-    Integer pageNum = 1;
+    Integer page = 1;
     Integer pageSize = 10;
 }
