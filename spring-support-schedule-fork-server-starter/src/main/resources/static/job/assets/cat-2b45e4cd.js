ae570cd003d883f2dc4d78768cd1d002720eeded
ACHTK <yemengzhidiandig@gmail.com>
2023-11-09 13:42:10 +0800

    no commit message

---
+++ spring-support-schedule-fork-server-starter/src/main/resources/static/job/assets/cat-2b45e4cd.js
@@ -0,0 +1,99 @@
+import{s as E,u as k,_ as O,r as d,o as v,e as y,w as S,g as I,a as w,c as j}from"./index-22fa9e8a.js";var C={};(function(f){(function(h,c){if(typeof f.nodeName!="string")c(f);else{var r={};c(r),h.AnsiUp=r.default}})(E,function(h){var c=this&&this.__makeTemplateObject||function(i,e){return Object.defineProperty?Object.defineProperty(i,"raw",{value:e}):i.raw=e,i},r;(function(i){i[i.EOS=0]="EOS",i[i.Text=1]="Text",i[i.Incomplete=2]="Incomplete",i[i.ESC=3]="ESC",i[i.Unknown=4]="Unknown",i[i.SGR=5]="SGR",i[i.OSCURL=6]="OSCURL"})(r||(r={}));var _=function(){function i(){this.VERSION="5.2.1",this.setup_palettes(),this._use_classes=!1,this.bold=!1,this.italic=!1,this.underline=!1,this.fg=this.bg=null,this._buffer="",this._url_whitelist={http:1,https:1},this._escape_html=!0}return Object.defineProperty(i.prototype,"use_classes",{get:function(){return this._use_classes},set:function(e){this._use_classes=e},enumerable:!1,configurable:!0}),Object.defineProperty(i.prototype,"url_whitelist",{get:function(){return this._url_whitelist},set:function(e){this._url_whitelist=e},enumerable:!1,configurable:!0}),Object.defineProperty(i.prototype,"escape_html",{get:function(){return this._escape_html},set:function(e){this._escape_html=e},enumerable:!1,configurable:!0}),i.prototype.setup_palettes=function(){var e=this;this.ansi_colors=[[{rgb:[0,0,0],class_name:"ansi-black"},{rgb:[187,0,0],class_name:"ansi-red"},{rgb:[0,187,0],class_name:"ansi-green"},{rgb:[187,187,0],class_name:"ansi-yellow"},{rgb:[0,0,187],class_name:"ansi-blue"},{rgb:[187,0,187],class_name:"ansi-magenta"},{rgb:[0,187,187],class_name:"ansi-cyan"},{rgb:[255,255,255],class_name:"ansi-white"}],[{rgb:[85,85,85],class_name:"ansi-bright-black"},{rgb:[255,85,85],class_name:"ansi-bright-red"},{rgb:[0,255,0],class_name:"ansi-bright-green"},{rgb:[255,255,85],class_name:"ansi-bright-yellow"},{rgb:[85,85,255],class_name:"ansi-bright-blue"},{rgb:[255,85,255],class_name:"ansi-bright-magenta"},{rgb:[85,255,255],class_name:"ansi-bright-cyan"},{rgb:[255,255,255],class_name:"ansi-bright-white"}]],this.palette_256=[],this.ansi_colors.forEach(function(p){p.forEach(function(b){e.palette_256.push(b)})});for(var t=[0,95,135,175,215,255],s=0;s<6;++s)for(var n=0;n<6;++n)for(var l=0;l<6;++l){var a={rgb:[t[s],t[n],t[l]],class_name:"truecolor"};this.palette_256.push(a)}for(var o=8,u=0;u<24;++u,o+=10){var g={rgb:[o,o,o],class_name:"truecolor"};this.palette_256.push(g)}},i.prototype.escape_txt_for_html=function(e){return this._escape_html?e.replace(/[&<>"']/gm,function(t){if(t==="&")return"&amp;";if(t==="<")return"&lt;";if(t===">")return"&gt;";if(t==='"')return"&quot;";if(t==="'")return"&#x27;"}):e},i.prototype.append_buffer=function(e){var t=this._buffer+e;this._buffer=t},i.prototype.get_next_packet=function(){var e={kind:r.EOS,text:"",url:""},t=this._buffer.length;if(t==0)return e;var s=this._buffer.indexOf("\x1B");if(s==-1)return e.kind=r.Text,e.text=this._buffer,this._buffer="",e;if(s>0)return e.kind=r.Text,e.text=this._buffer.slice(0,s),this._buffer=this._buffer.slice(s),e;if(s==0){if(t<3)return e.kind=r.Incomplete,e;var n=this._buffer.charAt(1);if(n!="["&&n!="]"&&n!="(")return e.kind=r.ESC,e.text=this._buffer.slice(0,1),this._buffer=this._buffer.slice(1),e;if(n=="["){this._csi_regex||(this._csi_regex=x(c([`
+                        ^                           # beginning of line
+                                                    #
+                                                    # First attempt
+                        (?:                         # legal sequence
+                          \x1B[                      # CSI
+                          ([<-?]?)              # private-mode char
+                          ([d;]*)                    # any digits or semicolons
+                          ([ -/]?               # an intermediate modifier
+                          [@-~])                # the command
+                        )
+                        |                           # alternate (second attempt)
+                        (?:                         # illegal sequence
+                          \x1B[                      # CSI
+                          [ -~]*                # anything legal
+                          ([\0-:])              # anything illegal
+                        )
+                    `],[`
+                        ^                           # beginning of line
+                                                    #
+                                                    # First attempt
+                        (?:                         # legal sequence
+                          \\x1b\\[                      # CSI
+                          ([\\x3c-\\x3f]?)              # private-mode char
+                          ([\\d;]*)                    # any digits or semicolons
+                          ([\\x20-\\x2f]?               # an intermediate modifier
+                          [\\x40-\\x7e])                # the command
+                        )
+                        |                           # alternate (second attempt)
+                        (?:                         # illegal sequence
+                          \\x1b\\[                      # CSI
+                          [\\x20-\\x7e]*                # anything legal
+                          ([\\x00-\\x1f:])              # anything illegal
+                        )
+                    `])));var l=this._buffer.match(this._csi_regex);if(l===null)return e.kind=r.Incomplete,e;if(l[4])return e.kind=r.ESC,e.text=this._buffer.slice(0,1),this._buffer=this._buffer.slice(1),e;l[1]!=""||l[3]!="m"?e.kind=r.Unknown:e.kind=r.SGR,e.text=l[2];var a=l[0].length;return this._buffer=this._buffer.slice(a),e}else if(n=="]"){if(t<4)return e.kind=r.Incomplete,e;if(this._buffer.charAt(2)!="8"||this._buffer.charAt(3)!=";")return e.kind=r.ESC,e.text=this._buffer.slice(0,1),this._buffer=this._buffer.slice(1),e;this._osc_st||(this._osc_st=m(c([`
+                        (?:                         # legal sequence
+                          (\x1B\\)                    # ESC                           |                           # alternate
+                          (\x07)                      # BEL (what xterm did)
+                        )
+                        |                           # alternate (second attempt)
+                        (                           # illegal sequence
+                          [\0-]                 # anything illegal
+                          |                           # alternate
+                          [\b-]                 # anything illegal
+                          |                           # alternate
+                          [-]                 # anything illegal
+                        )
+                    `],[`
+                        (?:                         # legal sequence
+                          (\\x1b\\\\)                    # ESC \\
+                          |                           # alternate
+                          (\\x07)                      # BEL (what xterm did)
+                        )
+                        |                           # alternate (second attempt)
+                        (                           # illegal sequence
+                          [\\x00-\\x06]                 # anything illegal
+                          |                           # alternate
+                          [\\x08-\\x1a]                 # anything illegal
+                          |                           # alternate
+                          [\\x1c-\\x1f]                 # anything illegal
+                        )
+                    `]))),this._osc_st.lastIndex=0;{var o=this._osc_st.exec(this._buffer);if(o===null)return e.kind=r.Incomplete,e;if(o[3])return e.kind=r.ESC,e.text=this._buffer.slice(0,1),this._buffer=this._buffer.slice(1),e}{var u=this._osc_st.exec(this._buffer);if(u===null)return e.kind=r.Incomplete,e;if(u[3])return e.kind=r.ESC,e.text=this._buffer.slice(0,1),this._buffer=this._buffer.slice(1),e}this._osc_regex||(this._osc_regex=x(c([`
+                        ^                           # beginning of line
+                                                    #
+                        \x1B]8;                    # OSC Hyperlink
+                        [ -:<-~]*       # params (excluding ;)
+                        ;                           # end of params
+                        ([!-~]{0,512})        # URL capture
+                        (?:                         # ST
+                          (?:\x1B\\)                  # ESC                           |                           # alternate
+                          (?:\x07)                    # BEL (what xterm did)
+                        )
+                        ([ -~]+)              # TEXT capture
+                        \x1B]8;;                   # OSC Hyperlink End
+                        (?:                         # ST
+                          (?:\x1B\\)                  # ESC                           |                           # alternate
+                          (?:\x07)                    # BEL (what xterm did)
+                        )
+                    `],[`
+                        ^                           # beginning of line
+                                                    #
+                        \\x1b\\]8;                    # OSC Hyperlink
+                        [\\x20-\\x3a\\x3c-\\x7e]*       # params (excluding ;)
+                        ;                           # end of params
+                        ([\\x21-\\x7e]{0,512})        # URL capture
+                        (?:                         # ST
+                          (?:\\x1b\\\\)                  # ESC \\
+                          |                           # alternate
+                          (?:\\x07)                    # BEL (what xterm did)
+                        )
+                        ([\\x20-\\x7e]+)              # TEXT capture
+                        \\x1b\\]8;;                   # OSC Hyperlink End
+                        (?:                         # ST
+                          (?:\\x1b\\\\)                  # ESC \\
+                          |                           # alternate
+                          (?:\\x07)                    # BEL (what xterm did)
+                        )
+                    `])));var l=this._buffer.match(this._osc_regex);if(l===null)return e.kind=r.ESC,e.text=this._buffer.slice(0,1),this._buffer=this._buffer.slice(1),e;e.kind=r.OSCURL,e.url=l[1],e.text=l[2];var a=l[0].length;return this._buffer=this._buffer.slice(a),e}else if(n=="(")return e.kind=r.Unknown,this._buffer=this._buffer.slice(3),e}},i.prototype.ansi_to_html=function(e){this.append_buffer(e);for(var t=[];;){var s=this.get_next_packet();if(s.kind==r.EOS||s.kind==r.Incomplete)break;s.kind==r.ESC||s.kind==r.Unknown||(s.kind==r.Text?t.push(this.transform_to_html(this.with_state(s))):s.kind==r.SGR?this.process_ansi(s):s.kind==r.OSCURL&&t.push(this.process_hyperlink(s)))}return t.join("")},i.prototype.with_state=function(e){return{bold:this.bold,italic:this.italic,underline:this.underline,fg:this.fg,bg:this.bg,text:e.text}},i.prototype.process_ansi=function(e){for(var t=e.text.split(";");t.length>0;){var s=t.shift(),n=parseInt(s,10);if(isNaN(n)||n===0)this.fg=this.bg=null,this.bold=!1,this.italic=!1,this.underline=!1;else if(n===1)this.bold=!0;else if(n===3)this.italic=!0;else if(n===4)this.underline=!0;else if(n===22)this.bold=!1;else if(n===23)this.italic=!1;else if(n===24)this.underline=!1;else if(n===39)this.fg=null;else if(n===49)this.bg=null;else if(n>=30&&n<38)this.fg=this.ansi_colors[0][n-30];else if(n>=40&&n<48)this.bg=this.ansi_colors[0][n-40];else if(n>=90&&n<98)this.fg=this.ansi_colors[1][n-90];else if(n>=100&&n<108)this.bg=this.ansi_colors[1][n-100];else if((n===38||n===48)&&t.length>0){var l=n===38,a=t.shift();if(a==="5"&&t.length>0){var o=parseInt(t.shift(),10);o>=0&&o<=255&&(l?this.fg=this.palette_256[o]:this.bg=this.palette_256[o])}if(a==="2"&&t.length>2){var u=parseInt(t.shift(),10),g=parseInt(t.shift(),10),p=parseInt(t.shift(),10);if(u>=0&&u<=255&&g>=0&&g<=255&&p>=0&&p<=255){var b={rgb:[u,g,p],class_name:"truecolor"};l?this.fg=b:this.bg=b}}}}},i.prototype.transform_to_html=function(e){var t=e.text;if(t.length===0||(t=this.escape_txt_for_html(t),!e.bold&&!e.italic&&!e.underline&&e.fg===null&&e.bg===null))return t;var s=[],n=[],l=e.fg,a=e.bg;e.bold&&s.push("font-weight:bold"),e.italic&&s.push("font-style:italic"),e.underline&&s.push("text-decoration:underline"),this._use_classes?(l&&(l.class_name!=="truecolor"?n.push(l.class_name+"-fg"):s.push("color:rgb("+l.rgb.join(",")+")")),a&&(a.class_name!=="truecolor"?n.push(a.class_name+"-bg"):s.push("background-color:rgb("+a.rgb.join(",")+")"))):(l&&s.push("color:rgb("+l.rgb.join(",")+")"),a&&s.push("background-color:rgb("+a.rgb+")"));var o="",u="";return n.length&&(o=' class="'+n.join(" ")+'"'),s.length&&(u=' style="'+s.join(";")+'"'),"<span"+u+o+">"+t+"</span>"},i.prototype.process_hyperlink=function(e){var t=e.url.split(":");if(t.length<1||!this._url_whitelist[t[0]])return"";var s='<a href="'+this.escape_txt_for_html(e.url)+'">'+this.escape_txt_for_html(e.text)+"</a>";return s},i}();function x(i){var e=i.raw[0],t=/^\s+|\s+\n|\s*#[\s\S]*?\n|\n/gm,s=e.replace(t,"");return new RegExp(s)}function m(i){var e=i.raw[0],t=/^\s+|\s+\n|\s*#[\s\S]*?\n|\n/gm,s=e.replace(t,"");return new RegExp(s,"g")}Object.defineProperty(h,"__esModule",{value:!0}),h.default=_})})(C);const B=k(C);new B;const R={data(){return{form:{},showDialog:!1,loadingStatus:!1,returnResult:{logContent:""}}},mounted:function(){},methods:{open(){return this.showDialog=!0,this},setData(f){this.form=f,this.initial()},initial(){this.loadingStatus=!0,this.$API.scheduler.logDetailCat.get({logId:this.form.id,fromLineNum:0}).then(f=>{var h;if(f.code==="00000"&&(f==null?void 0:f.data.code)==200)return this.returnResult=f.data.content,!1;this.$message.error((h=f==null?void 0:f.data)==null?void 0:h.msg)}).finally(()=>this.loadingStatus=!1)}}},T={class:"container"},U={key:1,style:{height:"100%"}};function L(f,h,c,r,_,x){const m=d("el-empty"),i=d("highlightjs"),e=d("el-skeleton"),t=d("el-dialog");return v(),y(t,{modelValue:_.showDialog,"onUpdate:modelValue":h[0]||(h[0]=s=>_.showDialog=s),draggable:"",width:"70%","close-on-click-modal":!1,"destroy-on-close":!0},{default:S(()=>[I("div",T,[w(e,{animated:!0,loading:_.loadingStatus},{default:S(()=>[_.returnResult?(v(),j("div",U,[w(i,{language:"yml",autodetect:!1,code:_.returnResult.logContent.replaceAll("<br>",`\r
+`),style:{"overflow-y":"auto",height:"600px","font-size":"14px","font-family":"'幼圆', YouYuan, simsun, sans-serif, Microsoft YaHei, Consolas, Monaco, Menlo, Consolas, 'Courier New', monospace"}},null,8,["code"])])):(v(),y(m,{key:0,description:"暂无日志"}))]),_:1},8,["loading"])])]),_:1},8,["modelValue"])}const q=O(R,[["render",L],["__file","Z:/workspace/vue-support-parent-starter/vue-support-scheduler-starter/src/views/scheduler/joblog/cat.vue"]]);export{q as default};
