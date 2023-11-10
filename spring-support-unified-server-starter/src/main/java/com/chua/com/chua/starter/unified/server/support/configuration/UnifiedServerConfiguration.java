2ce2f134a80d42d33ca478767cdc3b9102451dec
ACHTK <yemengzhidiandig@gmail.com>
2023-11-10 17:27:02 +0800

    fix 1.设备接口 2.云接口

---
+++ spring-support-unified-server-starter/src/main/java/com/chua/com/chua/starter/unified/server/support/configuration/UnifiedServerConfiguration.java
@@ -0,0 +1,12 @@
+package com.chua.com.chua.starter.unified.server.support.configuration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * 统一服务器配置
+ *
+ * @author CH
+ */
+@EnableConfigurationProperties(UnifiedServerConfiguration.class)
+public class UnifiedServerConfiguration {
+}
