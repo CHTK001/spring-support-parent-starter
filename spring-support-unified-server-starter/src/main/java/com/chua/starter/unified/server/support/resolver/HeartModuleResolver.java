931a8ae09a5dbb11dba5074c769aecddc40949e8
ACHTK <yemengzhidiandig@gmail.com>
2023-11-20 19:17:42 +0800

    fix 1.设备接口 2.云接口

--- spring-support-unified-server-starter/src/main/java/com/chua/starter/unified/server/support/resolver/HeartModuleResolver.java
+++ spring-support-unified-server-starter/src/main/java/com/chua/starter/unified/server/support/resolver/HeartModuleResolver.java
@@ -1,17 +1,14 @@
 package com.chua.starter.unified.server.support.resolver;
 
-import com.alibaba.fastjson2.JSONObject;
 import com.chua.common.support.annotations.Spi;
 import com.chua.common.support.protocol.boot.BootRequest;
 import com.chua.common.support.protocol.boot.BootResponse;
 import com.chua.common.support.protocol.boot.CommandType;
+import com.chua.common.support.utils.ThreadUtils;
 import com.chua.starter.unified.server.support.properties.UnifiedServerProperties;
-import org.springframework.data.redis.core.RedisTemplate;
+import com.chua.starter.unified.server.support.service.UnifiedExecuterItemService;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
-
-import static com.chua.starter.unified.server.support.constant.UnifiedConstant.EXECUTOR_NAME;
 
 /**
  * 心脏模块分解器
@@ -22,20 +19,29 @@ import static com.chua.starter.unified.server.support.constant.UnifiedConstant.E
 public class HeartModuleResolver implements ModuleResolver{
 
     @Resource
-    private RedisTemplate<String, BootRequest> redisTemplate;
+    private UnifiedServerProperties unifiedServerProperties;
 
     @Resource
-    private UnifiedServerProperties unifiedServerProperties;
+    private UnifiedExecuterItemService unifiedExecuterItemService;
+
     @Override
     public BootResponse resolve(BootRequest request) {
         CommandType commandType = request.getCommandType();
         if(commandType == CommandType.PING) {
-            String appName = request.getAppName();
-            JSONObject jsonObject = JSONObject.parseObject(request.getContent());
-            redisTemplate.opsForValue()
-                    .set(EXECUTOR_NAME + appName + ":" + jsonObject.getString("host") + "_" + jsonObject.getString("port"), request, unifiedServerProperties.getKeepAliveTimeout(), TimeUnit.SECONDS);
-            return BootResponse.builder().data(BootResponse.DataDTO.builder().commandType(CommandType.PONG).build()).build();
+            ThreadUtils.newStaticThreadPool().execute(() -> {
+                try {
+                    unifiedExecuterItemService.checkHeart(request);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            return BootResponse.builder()
+                    .data(BootResponse.DataDTO.builder().commandType(CommandType.PONG).build()).build();
         }
         return null;
     }
+
+
+
+
 }
