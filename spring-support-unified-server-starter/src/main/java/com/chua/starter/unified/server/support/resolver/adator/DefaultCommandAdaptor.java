4b4c38a9c5251f6d851a63d70bb13c82589404d8
ACHTK <yemengzhidiandig@gmail.com>
2023-11-20 19:33:22 +0800

    no commit message

---
+++ spring-support-unified-server-starter/src/main/java/com/chua/starter/unified/server/support/resolver/adator/DefaultCommandAdaptor.java
@@ -0,0 +1,20 @@
+package com.chua.starter.unified.server.support.resolver.adator;
+
+
+import com.chua.common.support.annotations.SpiDefault;
+import com.chua.common.support.protocol.boot.BootRequest;
+import com.chua.common.support.protocol.boot.BootResponse;
+
+/**
+ * 默认命令适配器
+ *
+ * @author CH
+ * @since 2023/11/16
+ */
+@SpiDefault
+public class DefaultCommandAdaptor implements ConfigCommandAdaptor, ExecutorCommandAdaptor{
+    @Override
+    public BootResponse resolve(BootRequest request) {
+        return null;
+    }
+}
