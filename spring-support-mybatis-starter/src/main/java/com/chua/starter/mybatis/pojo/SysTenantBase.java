3da9a34cb5e28ccc730ac257f47591f8770063d2
ACHTK <yemengzhidiandig@gmail.com>
2024-01-11 11:58:59 +0800

    版本升级

--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/pojo/SysTenantBase.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/pojo/SysTenantBase.java
@@ -1,6 +1,8 @@
 package com.chua.starter.mybatis.pojo;
 
 import com.chua.common.support.datasource.annotation.Column;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -17,5 +19,7 @@ public class SysTenantBase extends SysBase{
      * 多租户编号
      */
     @Column(defaultValue = "0", comment = "租户ID", refresh = true)
+    @Schema(title = "租户ID")
+    @ApiModelProperty("租户ID")
     private Long tenantId;
 }
