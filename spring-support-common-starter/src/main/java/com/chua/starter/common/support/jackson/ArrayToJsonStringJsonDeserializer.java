411351bfb347e12d215829c7884337ab35bf4ae6
ACHTK <yemengzhidiandig@gmail.com>
2024-01-17 21:14:50 +0800

    1.添加用户设置

--- spring-support-common-starter/src/main/java/com/chua/starter/common/support/jackson/ArrayToJsonStringJsonDeserializer.java
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/jackson/ArrayToJsonStringJsonDeserializer.java
@@ -3,11 +3,14 @@ package com.chua.starter.common.support.jackson;
 import com.chua.common.support.json.Json;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.*;
 
 /**
@@ -31,18 +34,12 @@ public class ArrayToJsonStringJsonDeserializer extends JsonDeserializer<Object> 
      * @return {@link Object}
      */
     private Object treeNodeToValue(TreeNode treeNode) {
-        if (treeNode.isArray()) {
-            return treeNodeToArray(treeNode);
+        Object o = treeNodeToValueValue(treeNode);
+        if(null == o) {
+            return null;
         }
 
-        if (treeNode.isObject()) {
-            return treeNodeToObject(treeNode);
-        }
-
-        if (treeNode.isValueNode()) {
-            return treeNode.toString();
-        }
-        return null;
+        return Json.toJson(o);
     }
     /**
      * 树节点到值
@@ -60,9 +57,51 @@ public class ArrayToJsonStringJsonDeserializer extends JsonDeserializer<Object> 
         }
 
         if (treeNode.isValueNode()) {
-            return treeNode.toString();
+            return treeNodeToNewValue(treeNode);
+        }
+
+        if(treeNode.isMissingNode()) {
+            return null;
         }
         return null;
+    }
+
+    private Object treeNodeToNewValue(TreeNode treeNode) {
+        JsonToken token = treeNode.asToken();
+        if(token.isBoolean()) {
+            return Boolean.valueOf(treeNode.toString());
+        }
+
+
+        if(token.isNumeric()) {
+            JsonParser.NumberType numberType = treeNode.numberType();
+            if(numberType == JsonParser.NumberType.INT) {
+                return Integer.valueOf(treeNode.toString());
+            }
+
+            if(numberType == JsonParser.NumberType.LONG) {
+                return Long.valueOf(treeNode.toString());
+            }
+
+            if(numberType == JsonParser.NumberType.FLOAT) {
+                return Float.valueOf(treeNode.toString());
+            }
+
+            if(numberType == JsonParser.NumberType.DOUBLE) {
+                return Double.valueOf(treeNode.toString());
+            }
+
+            if(numberType == JsonParser.NumberType.BIG_DECIMAL) {
+                return new BigDecimal(treeNode.toString());
+            }
+
+            if(numberType == JsonParser.NumberType.BIG_INTEGER) {
+                return new BigInteger(treeNode.toString());
+            }
+        }
+
+        return null;
+
     }
 
     private Object treeNodeToObjectValue(TreeNode treeNode) {
@@ -73,14 +112,6 @@ public class ArrayToJsonStringJsonDeserializer extends JsonDeserializer<Object> 
         });
         return rs;
     }
-    private Object treeNodeToObject(TreeNode treeNode) {
-        Iterator<String> stringIterator = treeNode.fieldNames();
-        Map<String, Object> rs = new LinkedHashMap<>();
-        stringIterator.forEachRemaining(it -> {
-            rs.put(it, treeNodeToValueValue(treeNode.get(it)));
-        });
-        return Json.toJson(rs);
-    }
 
     private Object treeNodeToArrayValue(TreeNode treeNode) {
         int size = treeNode.size();
@@ -90,14 +121,5 @@ public class ArrayToJsonStringJsonDeserializer extends JsonDeserializer<Object> 
             rs.add(treeNodeToValue(treeNode1));
         }
         return rs;
-    }
-    private Object treeNodeToArray(TreeNode treeNode) {
-        int size = treeNode.size();
-        List<Object> rs = new LinkedList<>();
-        for (int i = 0; i < size; i++) {
-            TreeNode treeNode1 = treeNode.get(i);
-            rs.add(treeNodeToValueValue(treeNode1));
-        }
-        return Json.toJson(rs);
     }
 }
