b2abf982ada2a8bf0ac30abceae728b08e55765d
ACHTK <yemengzhidiandig@gmail.com>
2024-01-18 17:26:49 +0800

    1、系统配置

--- spring-support-socketio-starter/src/main/java/com/chua/socketio/support/SocketIOListener.java
+++ spring-support-socketio-starter/src/main/java/com/chua/socketio/support/SocketIOListener.java
@@ -1,27 +1,10 @@
 package com.chua.socketio.support;
 
-import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.annotation.OnConnect;
-import com.corundumstudio.socketio.annotation.OnDisconnect;
-
 /**
  * 监听
  *
  * @author CH
  */
 public interface SocketIOListener {
-    /**
-     * 建立连接
-     *
-     * @param client 客户端
-     */
-    @OnConnect
-    void onConnect(SocketIOClient client);
-    /**
-     * 建立连接
-     *
-     * @param client 客户端
-     */
-    @OnDisconnect
-    void onDisConnect(SocketIOClient client);
+
 }
