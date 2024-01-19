cfa0d7fd98168f24f0f4700e35ba2ebf49a6ac6f
ACHTK <yemengzhidiandig@gmail.com>
2024-01-19 20:32:59 +0800

    1.添加用户设置

--- spring-support-common-starter/src/main/java/com/chua/starter/common/support/filter/CryptoFilter.java
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/filter/CryptoFilter.java
@@ -4,12 +4,8 @@ import com.alibaba.fastjson2.filter.ValueFilter;
 import com.chua.common.support.crypto.Codec;
 import com.chua.common.support.crypto.CryptoModule;
 import com.chua.common.support.crypto.CryptoType;
-import com.chua.common.support.spi.ServiceProvider;
-import com.chua.common.support.utils.PrivacyUtils;
 import com.chua.common.support.utils.StringUtils;
 import com.chua.starter.common.support.annotations.Crypto;
-import com.chua.starter.common.support.annotations.PrivacyEncrypt;
-import com.chua.starter.common.support.rule.PrivacyTypeEnum;
 import com.google.common.base.Strings;
 
 import java.lang.reflect.Field;
@@ -38,11 +34,10 @@ public class CryptoFilter implements ValueFilter {
             String key = crypto.key();
             String origin = value.toString();
             if (!Strings.isNullOrEmpty(origin) && null != cryptoType && StringUtils.isNotEmpty(key) && null != cryptoModule) {
-                Codec codec = ServiceProvider.of(Codec.class).getExtension(cryptoModule);
                 if(cryptoModule == CryptoModule.DECODE) {
-                    return codec.encodeHex(origin, key);
+                    return Codec.build(cryptoModule.name(), key).encodeHex(origin);
                 }
-                return codec.decodeHex(origin, key);
+                return Codec.build(cryptoModule.name(), key).decodeHex(origin);
             }
             return origin;
         } catch (Exception ignored) {
