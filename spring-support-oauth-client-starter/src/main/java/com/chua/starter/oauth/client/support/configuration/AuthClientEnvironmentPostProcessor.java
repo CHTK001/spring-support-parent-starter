14ed4df46e1f6a4b49f8b92b9e7c151896db70c7
Administrator <1@q>
2024-01-29 19:32:13 +0800

    1.三方登录

--- spring-support-oauth-client-starter/src/main/java/com/chua/starter/oauth/client/support/configuration/AuthClientEnvironmentPostProcessor.java
+++ spring-support-oauth-client-starter/src/main/java/com/chua/starter/oauth/client/support/configuration/AuthClientEnvironmentPostProcessor.java
@@ -1,7 +1,5 @@
 package com.chua.starter.oauth.client.support.configuration;
 
-import com.chua.common.support.utils.StringUtils;
-import com.chua.starter.oauth.client.support.properties.AuthClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -21,9 +19,6 @@ public class AuthClientEnvironmentPostProcessor implements EnvironmentPostProces
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         Properties properties = new Properties();
-        if(StringUtils.isBlank(environment.resolvePlaceholders("${" + AuthClientProperties.PRE + ".address:}"))) {
-            properties.setProperty("plugin.oauth.protocol", "Static");
-        }
         PropertiesPropertySource propertiesPropertySource = new PropertiesPropertySource("oauth", properties);
         MutablePropertySources propertySources = environment.getPropertySources();
         propertySources.addLast(propertiesPropertySource);
