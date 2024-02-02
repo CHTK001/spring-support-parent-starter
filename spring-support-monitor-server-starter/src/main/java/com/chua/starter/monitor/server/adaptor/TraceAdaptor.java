000008cc9ed1964eef43d3d079960044542446d8
Administrator <1@q>
2024-02-02 21:22:53 +0800

    1.监听接口

---
+++ spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/adaptor/TraceAdaptor.java
@@ -0,0 +1,29 @@
+package com.chua.starter.monitor.server.adaptor;
+
+import com.chua.common.support.utils.StringUtils;
+import com.chua.socketio.support.session.SocketSessionTemplate;
+import com.chua.starter.monitor.request.MonitorRequest;
+
+import javax.annotation.Resource;
+
+/**
+ * jvm适配器
+ *
+ * @author CH
+ * @version 1.0.0
+ * @since 2024/02/01
+ */
+public class TraceAdaptor implements Adaptor<MonitorRequest> {
+
+    @Resource
+    private SocketSessionTemplate socketSessionTemplate;
+    @Override
+    public void doAdaptor(MonitorRequest request) {
+        socketSessionTemplate.send("trace", StringUtils.format("[{}({}:{})] -> {}", request.getAppName(), request.getServerHost() , request.getServerPort(), request.getData()));
+    }
+
+    @Override
+    public Class<MonitorRequest> getType() {
+        return MonitorRequest.class;
+    }
+}
