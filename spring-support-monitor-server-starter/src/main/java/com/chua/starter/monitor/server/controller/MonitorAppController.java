e69b4e96b880e6d0bb72a8d34e190741359f08f5
ACHTK <yemengzhidiandig@gmail.com>
2024-02-03 15:27:11 +0800

    1.天气

--- spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/controller/MonitorAppController.java
+++ spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/controller/MonitorAppController.java
@@ -4,6 +4,7 @@ package com.chua.starter.monitor.server.controller;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.chua.common.support.lang.code.ReturnPageResult;
+import com.chua.common.support.lang.code.ReturnResult;
 import com.chua.starter.monitor.request.MonitorRequest;
 import com.chua.starter.monitor.server.entity.MonitorApp;
 import com.chua.starter.monitor.server.factory.MonitorServerFactory;
@@ -56,12 +57,29 @@ public class MonitorAppController extends AbstractSwaggerUpdateController<Monito
         return PageResultUtils.ok(page1);
     }
 
+    /**
+     * 分页查询数据
+     *
+     * @param entity 结果
+     * @return 分页结果
+     */
+    @ResponseBody
+    @Operation(summary = "查询基础数据")
+    @GetMapping("list")
+    public ReturnResult<List<MonitorApp>> list(PageRequest<MonitorApp> page, @Valid MonitorApp entity, @Ignore BindingResult bindingResult) {
+        List<MonitorApp> page1 = getService().list(Wrappers.lambdaQuery(entity));
+        mergePage(page1);
+        return ReturnResult.ok(page1);
+    }
+
     private void mergePage(Page<MonitorApp> page1) {
+        mergePage(page1.getRecords());
+    }
+    private void mergePage(List<MonitorApp> page1) {
         Map<String, List<MonitorRequest>> appHeart = monitorServerFactory.getHeart();
-        for (MonitorApp record : page1.getRecords()) {
+        for (MonitorApp record : page1) {
             record.setMonitorRequests(appHeart.get(record.getMonitorAppname()));
         }
-
     }
 
 
