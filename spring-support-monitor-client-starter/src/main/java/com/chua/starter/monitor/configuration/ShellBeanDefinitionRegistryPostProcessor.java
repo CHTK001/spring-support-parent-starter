406ed789c72570f937c1ed41feeeda5f4ed0d917
Administrator <1@q>
2024-02-06 16:49:42 +0800

    1.注册中心

---
+++ spring-support-monitor-client-starter/src/main/java/com/chua/starter/monitor/configuration/ShellBeanDefinitionRegistryPostProcessor.java
@@ -0,0 +1,22 @@
+package com.chua.starter.monitor.configuration;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+import static com.chua.starter.monitor.configuration.ShellConfiguration.shell;
+
+public class ShellBeanDefinitionRegistryPostProcessor implements BeanPostProcessor, BeanDefinitionRegistryPostProcessor {
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        shell.register(bean);
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+}
--- spring-support-monitor-client-starter/src/main/java/com/chua/starter/monitor/configuration/ShellConfiguration.java
+++ spring-support-monitor-client-starter/src/main/java/com/chua/starter/monitor/configuration/ShellConfiguration.java
@@ -13,26 +13,13 @@ import com.chua.common.support.shell.mapping.DelegateCommand;
 import com.chua.common.support.shell.mapping.HelpCommand;
 import com.chua.common.support.shell.mapping.SystemCommand;
 import com.chua.common.support.spi.ServiceProvider;
-import com.chua.starter.common.support.utils.RequestUtils;
 import com.chua.starter.monitor.shell.command.CfrCommand;
 import com.chua.starter.monitor.shell.command.SpringCommand;
 import com.chua.starter.monitor.shell.resolver.Resolver;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.util.WebAppRootListener;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Collections;
 
 /**
  * 外壳配置
@@ -41,7 +28,7 @@ import java.util.Collections;
  * @version 1.0.0
  * @since 2024/02/06
  */
-public class ShellConfiguration implements ServletContextInitializer, BeanFactoryAware, BeanPostProcessor, BeanDefinitionRegistryPostProcessor {
+public class ShellConfiguration implements BeanFactoryAware {
 
     public static BaseShell shell = new WebShell();
     static {
@@ -74,11 +61,6 @@ public class ShellConfiguration implements ServletContextInitializer, BeanFactor
                         .build())
                 .build();
     }
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        shell.register(bean);
-        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
-    }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
@@ -93,47 +75,6 @@ public class ShellConfiguration implements ServletContextInitializer, BeanFactor
         }
         this.protocolServer = this.beanFactory.getBean(ProtocolServer.class);
         this.protocolServer.addListen(this);
-    }
-
-
-
-
-    @Bean
-    public FilterRegistrationBean<ShellFilter> shellFilter() {
-        FilterRegistrationBean<ShellFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new ShellFilter());
-        registrationBean.setAsyncSupported(true);
-        registrationBean.setOrder(1);
-        registrationBean.setUrlPatterns(Collections.singleton("/*"));
-        return registrationBean;
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        servletContext.addListener(WebAppRootListener.class);
-        servletContext.setInitParameter("org.apache.tomcat.websocket.textBufferSize", "52428800");
-        servletContext.setInitParameter("org.apache.tomcat.websocket.binaryBufferSize", "52428800");
-    }
-
-
-
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-
-    }
-
-    @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-
-    }
-
-    static class ShellFilter implements Filter {
-        @Override
-        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
-            HttpServletRequest req = (HttpServletRequest) servletRequest;
-            req.getSession().setAttribute("ip", RequestUtils.getIpAddress(req));
-            filterChain.doFilter(servletRequest, servletResponse);
-        }
     }
 
 }
