f453ecd4eebb1563de7ff4a7228bdc5028130030
Administrator <1@q>
2024-02-20 11:31:30 +0800

    1.限流

---
+++ spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/mapper/MonitorLimitMapper.java
@@ -0,0 +1,9 @@
+package com.chua.starter.monitor.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chua.starter.monitor.server.entity.MonitorLimit;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MonitorLimitMapper extends BaseMapper<MonitorLimit> {
+}
