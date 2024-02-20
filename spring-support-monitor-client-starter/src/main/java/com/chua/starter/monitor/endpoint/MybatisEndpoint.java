05fdbdd69d624e46d21fd0cf7e7144042ca1660a
Administrator <1@q>
2024-02-20 17:13:03 +0800

    1.mybatis管理

---
+++ spring-support-monitor-client-starter/src/main/java/com/chua/starter/monitor/endpoint/MybatisEndpoint.java
@@ -0,0 +1,28 @@
+package com.chua.starter.monitor.endpoint;
+
+import com.chua.starter.monitor.mybatis.SupportInjector;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+
+import javax.annotation.Resource;
+
+/**
+ * redis端点
+ *
+ * @author CH
+ * @version 1.0.0
+ * @since 2024/02/02
+ */
+
+@ConditionalOnBean(SupportInjector.class)
+@WebEndpoint(id = "mybatis")
+public class MybatisEndpoint {
+    @Resource
+    private SupportInjector supportInjector;;
+    @ReadOperation
+    public Object read() {
+        return supportInjector.getStatementMap().keySet();
+    }
+
+}
