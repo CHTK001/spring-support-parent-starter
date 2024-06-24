f9c01b4a152fb15b770c40e48ccd044b98ab969a
Administrator <1@q>
2024-06-24 17:32:56 +0800

    1.升级jdk版本为21

--- spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/service/impl/IptablesServiceImpl.java
+++ spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/service/impl/IptablesServiceImpl.java
@@ -1,12 +1,18 @@
 package com.chua.starter.monitor.server.service.impl;
 
 import com.chua.common.support.geo.GeoCity;
+import com.chua.common.support.geo.GeoSetting;
 import com.chua.common.support.geo.IpPosition;
 import com.chua.common.support.lang.code.ReturnResult;
 import com.chua.common.support.spi.ServiceProvider;
 import com.chua.common.support.utils.StringUtils;
 import com.chua.starter.common.support.properties.IpProperties;
 import com.chua.starter.monitor.server.service.IptablesService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,9 +22,13 @@ import org.springframework.stereotype.Service;
  * @since 2024/01/19
  */
 @Service
-public class IptablesServiceImpl implements IptablesService {
+public class IptablesServiceImpl implements IptablesService, ApplicationContextAware {
 
+    @Resource
     private IpProperties ipProperties;
+
+    private IpPosition ipPosition;
+
     /**
      * 翻译地址
      * @param address 地址
@@ -30,7 +40,19 @@ public class IptablesServiceImpl implements IptablesService {
             return ReturnResult.illegal("地址不能为空");
         }
 
-        IpPosition ipPosition = ServiceProvider.of(IpPosition.class).getExtension(ipProperties.getIpType());
         return null == ipPosition ? ReturnResult.illegal("解析失败") : ReturnResult.ok(ipPosition.getCity(address));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ipProperties = Binder.get(applicationContext.getEnvironment()).bindOrCreate("plugin.ip", IpProperties.class);
+
+        if(!ipProperties.isEnable()) {
+            return;
+        }
+        ipPosition = ServiceProvider.of(IpPosition.class).getNewExtension(ipProperties.getIpType(), GeoSetting.builder()
+                        .databaseFile(ipProperties.getDatabaseFile())
+                        .build()
+                );
     }
 }
