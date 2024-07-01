ea621c9e1ed607a975a2687a8585fdc1e2ba755c
Administrator <1@q>
2024-07-01 17:05:27 +0800

    1.重构协议

--- spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/service/impl/MonitorTerminalProjectServiceImpl.java
+++ spring-support-monitor-server-starter/src/main/java/com/chua/starter/monitor/server/service/impl/MonitorTerminalProjectServiceImpl.java
@@ -3,61 +3,59 @@ package com.chua.starter.monitor.server.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.chua.common.support.lang.code.ReturnResult;
 import com.chua.common.support.protocol.channel.Channel;
-import com.chua.common.support.session.Session;
 import com.chua.common.support.utils.StringUtils;
 import com.chua.socketio.support.session.SocketSessionTemplate;
-import com.chua.ssh.support.ssh.SshSession;
+import com.chua.ssh.support.ssh.SshClient;
 import com.chua.starter.monitor.server.entity.MonitorTerminalProject;
 import com.chua.starter.monitor.server.mapper.MonitorTerminalProjectMapper;
 import com.chua.starter.monitor.server.service.MonitorTerminalProjectService;
 import com.chua.starter.monitor.server.service.MonitorTerminalService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
 /**
- *
- *
- * @since 2024/6/19 
  * @author CH
+ * @since 2024/6/19
  */
 @Service
 public class
-MonitorTerminalProjectServiceImpl extends ServiceImpl<MonitorTerminalProjectMapper, MonitorTerminalProject> implements MonitorTerminalProjectService{
+MonitorTerminalProjectServiceImpl extends ServiceImpl<MonitorTerminalProjectMapper, MonitorTerminalProject> implements MonitorTerminalProjectService {
 
     @Resource
     private MonitorTerminalService monitorTerminalService;
     @Resource
     private SocketSessionTemplate socketSessionTemplate;
+
     @Override
     public ReturnResult<Boolean> runStartScript(MonitorTerminalProject monitorTerminalProject) {
-        Session session = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
-        if(null == session) {
+        SshClient sshClient = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
+        if (null == sshClient) {
             return ReturnResult.illegal("会话未开启");
         }
 
         String startScript = getStartScript(monitorTerminalProject);
-        if(null == startScript) {
+        if (null == startScript) {
             return ReturnResult.illegal("启动脚本为空");
         }
 
-        if(session instanceof SshSession terminalSession) {
-            Channel channel = terminalSession.openChannel("exec");
-            return ReturnResult.of(channel.execute(startScript, 1000 * 5));
-        }
-        return null;
+
+        Channel openChannel = sshClient.getSession().openChannel(String.valueOf(monitorTerminalProject.getTerminalId()), "exec");
+        return ReturnResult.of(openChannel.execute(startScript, 1000 * 5));
     }
 
     /**
      * 获取启动脚本
+     *
      * @param monitorTerminalProject 监控项目
      * @return 脚本
      */
     private String getStartScript(MonitorTerminalProject monitorTerminalProject) {
         String terminalProjectStartScript = monitorTerminalProject.getTerminalProjectStartScript();
-        if(StringUtils.isBlank(terminalProjectStartScript)) {
+        if (StringUtils.isBlank(terminalProjectStartScript)) {
             return null;
         }
 
-        if(terminalProjectStartScript.startsWith("/")) {
+        if (terminalProjectStartScript.startsWith("/")) {
             return terminalProjectStartScript;
         }
         return monitorTerminalProject.getTerminalProjectPath() + "/" + terminalProjectStartScript;
@@ -65,63 +63,54 @@ MonitorTerminalProjectServiceImpl extends ServiceImpl<MonitorTerminalProjectMapp
 
     @Override
     public ReturnResult<Boolean> runStopScript(MonitorTerminalProject monitorTerminalProject) {
-        Session session = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
-        if(null == session) {
+        SshClient sshClient = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
+        if (null == sshClient) {
             return ReturnResult.illegal("会话未开启");
         }
 
         String stopScript = getStopScript(monitorTerminalProject);
-        if(null == stopScript) {
+        if (null == stopScript) {
             return ReturnResult.illegal("启动脚本为空");
         }
-
-        if(session instanceof SshSession terminalSession) {
-            Channel channel = terminalSession.openChannel("exec");
-            return ReturnResult.of(channel.execute(stopScript, 1000 * 5));
-        }
-        return null;
+        Channel channel = sshClient.getSession().openChannel(String.valueOf(monitorTerminalProject.getTerminalId()), "exec");
+        return ReturnResult.of(channel.execute(stopScript, 1000 * 5));
     }
 
     @Override
     public ReturnResult<Boolean> logStart(MonitorTerminalProject monitorTerminalProject, String event) {
-        Session session = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
-        if(null == session) {
+        SshClient sshClient = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
+        if (null == sshClient) {
             return ReturnResult.illegal("会话未开启");
         }
 
         String logScript = getLogScript(monitorTerminalProject);
-        if(null == logScript) {
+        if (null == logScript) {
             return ReturnResult.illegal("日志文件不存在");
         }
 
-        if(session instanceof SshSession terminalSession) {
-            String logEventId = getLogEventId(monitorTerminalProject);
-            Channel channel = terminalSession.openChannel(logEventId, "shell");
-            channel.setListener(s -> socketSessionTemplate.send(event, s));
-            channel.execute("tail -f " + logScript, 1000);
-            return ReturnResult.success();
-        }
-        return null;
+        String logEventId = getLogEventId(monitorTerminalProject);
+        Channel channel = sshClient.getSession().openChannel(logEventId, "exec");
+
+        channel.setListener(s -> socketSessionTemplate.send(event, s));
+        channel.execute("tail -f " + logScript, 1000);
+        return ReturnResult.success();
     }
 
     @Override
     public ReturnResult<Boolean> logStop(MonitorTerminalProject monitorTerminalProject) {
-        Session session = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
-        if(null == session) {
+        SshClient sshClient = monitorTerminalService.getClient(monitorTerminalProject.getTerminalId() + "");
+        if (null == sshClient) {
             return ReturnResult.illegal("会话未开启");
         }
 
         String logScript = getLogScript(monitorTerminalProject);
-        if(null == logScript) {
+        if (null == logScript) {
             return ReturnResult.illegal("日志文件不存在");
         }
 
-        if(session instanceof SshSession terminalSession) {
-            String logEventId = getLogEventId(monitorTerminalProject);
-            terminalSession.closeChannel(logEventId);
-            return ReturnResult.success();
-        }
-        return null;
+        String logEventId = getLogEventId(monitorTerminalProject);
+        sshClient.getSession().closeChannel(logEventId);
+        return ReturnResult.success();
     }
 
     private String getLogEventId(MonitorTerminalProject monitorTerminalProject) {
@@ -130,32 +119,35 @@ MonitorTerminalProjectServiceImpl extends ServiceImpl<MonitorTerminalProjectMapp
 
     /**
      * 获取启动脚本
+     *
      * @param monitorTerminalProject 监控项目
      * @return 脚本
      */
     private String getLogScript(MonitorTerminalProject monitorTerminalProject) {
         String terminalProjectLog = monitorTerminalProject.getTerminalProjectLog();
-        if(StringUtils.isBlank(terminalProjectLog)) {
+        if (StringUtils.isBlank(terminalProjectLog)) {
             return null;
         }
 
-        if(terminalProjectLog.startsWith("/")) {
+        if (terminalProjectLog.startsWith("/")) {
             return terminalProjectLog;
         }
         return monitorTerminalProject.getTerminalProjectPath() + "/" + terminalProjectLog;
     }
+
     /**
      * 获取启动脚本
+     *
      * @param monitorTerminalProject 监控项目
      * @return 脚本
      */
     private String getStopScript(MonitorTerminalProject monitorTerminalProject) {
         String terminalProjectEndScript = monitorTerminalProject.getTerminalProjectEndScript();
-        if(StringUtils.isBlank(terminalProjectEndScript)) {
+        if (StringUtils.isBlank(terminalProjectEndScript)) {
             return null;
         }
 
-        if(terminalProjectEndScript.startsWith("/")) {
+        if (terminalProjectEndScript.startsWith("/")) {
             return terminalProjectEndScript;
         }
         return monitorTerminalProject.getTerminalProjectPath() + "/" + terminalProjectEndScript;
