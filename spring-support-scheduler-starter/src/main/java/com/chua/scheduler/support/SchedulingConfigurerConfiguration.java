6503270807edd965274051bcf3bdc5ae30d776c6
Administrator <1@q>
2024-08-01 16:46:54 +0800

    1.重构协议

---
+++ spring-support-scheduler-starter/src/main/java/com/chua/scheduler/support/SchedulingConfigurerConfiguration.java
@@ -0,0 +1,27 @@
+package com.chua.scheduler.support;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
+
+/**
+ * 定时任务配置
+ * @author CH
+ * @since 2024/8/1
+ */
+public class SchedulingConfigurerConfiguration {
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ScheduleCornChangeHandler scheduleCornChangeHandler(ScheduledAnnotationBeanPostProcessor processor) {
+        return new ScheduleCornChangeHandler(processor);
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ScheduleEndpoint scheduleEndpoint(ScheduleCornChangeHandler scheduleCornChangeHandler) {
+        return new ScheduleEndpoint(scheduleCornChangeHandler);
+    }
+}
