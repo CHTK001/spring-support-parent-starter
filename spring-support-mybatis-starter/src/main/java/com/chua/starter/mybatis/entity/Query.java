5afef2f92473c32aa644a5329fc40a622bba7d0f
Administrator <1@q>
2024-08-12 10:04:51 +0800

    1.重构协议

--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/entity/Query.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/entity/Query.java
@@ -2,11 +2,11 @@ package com.chua.starter.mybatis.entity;
 
 import com.chua.starter.common.support.annotations.RequestParamMapping;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 
 /**
  * page 查询
