fc2c69fd5dcfcb7c55f47df3617c6df9bcd74621
ACHTK <yemengzhidiandig@gmail.com>
2024-08-26 15:20:33 +0800

    1.添加系统版本

---
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/configuration/resolver/VersionArgumentResolver.java
@@ -0,0 +1,14 @@
+package com.chua.starter.common.support.configuration.resolver;
+
+/**
+ * 版本参数解析器
+ * @author CH
+ * @since 2024/8/26
+ */
+public interface VersionArgumentResolver {
+    /**
+     * 版本
+     * @return 版本
+     */
+    String version();
+}
