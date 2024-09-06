3ce1119e239879de1def7e93d6070c6e3e0dbe5b
ACHTK <yemengzhidiandig@gmail.com>
2024-09-06 17:18:20 +0800

    1.推送日志 2.推送sql

---
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/configuration/ProjectConfiguration.java
@@ -0,0 +1,38 @@
+package com.chua.starter.common.support.configuration;
+
+import com.chua.starter.common.support.project.Project;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+/**
+ * 项目配置
+ * @author CH
+ * @since 2024/9/6
+ */
+public class ProjectConfiguration implements BeanDefinitionRegistryPostProcessor, EnvironmentAware, CommandLineRunner, ApplicationContextAware {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        Project.getInstance().setEnvironment(environment);
+
+    }
+}
---
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/project/Project.java
@@ -0,0 +1,70 @@
+package com.chua.starter.common.support.project;
+
+import com.chua.common.support.utils.NumberUtils;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.core.env.Environment;
+
+/**
+ * 项目
+ * @author CH
+ * @since 2024/9/6
+ */
+@Data
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class Project {
+
+
+    private static final Project INSTANCE = new Project();
+    /**
+     * 应用名称
+     */
+    private String applicationName;
+
+    /**
+     * 应用端口
+     */
+    private Integer applicationPort;
+
+    /**
+     * 应用地址
+     */
+    private String applicationHost;
+
+    /**
+     * 应用环境
+     */
+    private String applicationActive;
+
+    /**
+     * 应用环境
+     */
+    private String applicationActiveInclude;
+    /**
+     * 上下文路径
+     */
+    private String contextPath;
+    /**
+     * 端点地址
+     */
+    private String endpointsUrl;
+    /**
+     * 环境
+     */
+    private Environment environment;
+
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+        this.applicationName = environment.getProperty("spring.application.name");
+        this.applicationPort = NumberUtils.toInt(environment.resolvePlaceholders("${server.port:8080}"));
+        this.applicationActive = environment.getProperty("spring.profiles.active", "default");
+        this.applicationActiveInclude = environment.getProperty("spring.profiles.include", "");
+        this.contextPath = environment.resolvePlaceholders("${server.servlet.context-path:}");
+        this.endpointsUrl = environment.resolvePlaceholders("${management.endpoints.web.base-path:/actuator}");
+    }
+
+    public static Project getInstance() {
+        return INSTANCE;
+    }
+
+}
