b27dbe67b956d5b5c2245ab56af9c7a7acd45e2e
ACHTK <yemengzhidiandig@gmail.com>
2024-09-14 16:56:09 +0800

    1.新增oss 2.新增动态代理

--- spring-support-report-server-starter/src/main/java/com/chua/report/server/starter/service/impl/MonitorSenderImpl.java
+++ spring-support-report-server-starter/src/main/java/com/chua/report/server/starter/service/impl/MonitorSenderImpl.java
@@ -13,16 +13,20 @@ import com.chua.common.support.protocol.request.Response;
 import com.chua.common.support.protocol.request.SenderRequest;
 import com.chua.common.support.spi.ServiceProvider;
 import com.chua.common.support.utils.ObjectUtils;
+import com.chua.common.support.utils.ThreadUtils;
 import com.chua.report.client.starter.endpoint.ModuleType;
 import com.chua.report.server.starter.entity.MonitorLog;
 import com.chua.report.server.starter.service.MonitorLogService;
 import com.chua.report.server.starter.service.MonitorSender;
 import com.chua.starter.common.support.project.Project;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
 
 /**
  * 监控发送
@@ -30,60 +34,66 @@ import java.util.Map;
  * @since 2024/9/12
  */
 @Service
+@Slf4j
 @RequiredArgsConstructor
-public class MonitorSenderImpl implements MonitorSender {
+public class MonitorSenderImpl implements MonitorSender, InitializingBean {
 
     final MonitorLogService monitorLogService;
+    private ExecutorService executorService;
 
     @Override
     public void upload(Object o, Discovery discovery, String params, ModuleType moduleType) {
-        Project project = getProject(discovery);
-        Codec codec = new AesCodec("1234567890123456".getBytes(StandardCharsets.UTF_8));
-        ProtocolSetting bootOption = ProtocolSetting.builder()
-                .host(project.getApplicationHost())
-                .port(Converter.createInteger(ObjectUtils.defaultIfNull(project.getClientProtocolEndpointPort(), project.getApplicationPort() + "10000")))
-                .heartbeat(false)
-                .codec(codec)
-                .build();
-        Protocol protocol = ServiceProvider.of(Protocol.class).getNewExtension(project.getClientProtocolEndpointProtocol(), bootOption);
-        MonitorLog monitorLog = new MonitorLog();
-        if(null == protocol) {
-            monitorLog.setLogCode(ReturnCode.RESOURCE_NOT_FOUND.getCode());
-            monitorLog.setLogMsg(ReturnCode.RESOURCE_NOT_FOUND.getMsg());
-            throw new RuntimeException("未找到对应协议");
-        }
+        executorService.execute(() -> {
+            Project project = getProject(discovery);
+            Codec codec = new AesCodec("1234567890123456".getBytes(StandardCharsets.UTF_8));
+            ProtocolSetting bootOption = ProtocolSetting.builder()
+                    .host(project.getApplicationHost())
+                    .port(Converter.createInteger(ObjectUtils.defaultIfNull(project.getClientProtocolEndpointPort(), project.getApplicationPort() + "10000")))
+                    .heartbeat(false)
+                    .codec(codec)
+                    .build();
+            Protocol protocol = ServiceProvider.of(Protocol.class).getNewExtension(project.getClientProtocolEndpointProtocol(), bootOption);
+            MonitorLog monitorLog = new MonitorLog();
+            if(null == protocol) {
+                monitorLog.setLogCode(ReturnCode.RESOURCE_NOT_FOUND.getCode());
+                monitorLog.setLogMsg(ReturnCode.RESOURCE_NOT_FOUND.getMsg());
+                throw new RuntimeException("未找到对应协议");
+            }
 
-        ProtocolClient protocolClient = protocol.createClient();
-        Response responseCode = protocolClient.sendRequestAndReply(SenderRequest.builder()
-                .url("/" + moduleType.name().toLowerCase())
-                .content(params)
-                .profile(project.getApplicationActive())
-                .appName(project.getApplicationName())
-                .build());
+            ProtocolClient protocolClient = protocol.createClient();
+            Response responseCode = protocolClient.sendRequestAndReply(SenderRequest.builder()
+                    .url("/" + moduleType.name().toLowerCase())
+                    .content(params)
+                    .profile(project.getApplicationActive())
+                    .appName(project.getApplicationName())
+                    .build());
 
 
-        try {
-            monitorLog.setLogCode(null == responseCode ? "-1" : String.valueOf(responseCode.code()));
-            monitorLog.setLogMsg(null == responseCode ? null : responseCode.getSource().toString());
-            monitorLog.setLogHost(project.getApplicationHost());
-            monitorLog.setLogPort(project.getApplicationPort());
-            monitorLog.setLogModuleType(moduleType.name());
-            monitorLog.setLogCommandType(CommandType.REQUEST);
-            monitorLog.setLogProfile(project.getApplicationActive());
-            monitorLog.setLogAppname(project.getApplicationName());
-            monitorLog.setLogContent(params);
+            try {
+                monitorLog.setLogCode(null == responseCode ? "-1" : String.valueOf(responseCode.code()));
+                monitorLog.setLogMsg(null == responseCode ? null : responseCode.getSource().toString());
+                monitorLog.setLogHost(project.getApplicationHost());
+                monitorLog.setLogPort(project.getApplicationPort());
+                monitorLog.setLogModuleType(moduleType.name());
+                monitorLog.setLogCommandType(CommandType.REQUEST);
+                monitorLog.setLogProfile(project.getApplicationActive());
+                monitorLog.setLogAppname(project.getApplicationName());
+                monitorLog.setLogContent(params);
 
-            monitorLogService.save(monitorLog);
-        } catch (Exception ignored) {
-        }
-        if(!responseCode.isSuccessful()) {
-            throw new RuntimeException(responseCode.message());
-        }
+                monitorLogService.save(monitorLog);
+            } catch (Exception ignored) {
+            }
+        });
     }
 
     @Override
     public Project getProject(Discovery discovery) {
         Map<String, String> metadata = discovery.getMetadata();
         return new Project(metadata);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.executorService = ThreadUtils.newVirtualThreadExecutor();
     }
 }
