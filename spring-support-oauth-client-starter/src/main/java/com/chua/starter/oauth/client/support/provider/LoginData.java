b0ce410e74efef0a649821eed26986daef6972af
ACHTK <yemengzhidiandig@gmail.com>
2024-11-29 09:27:00 +0800

    feat(spring-support-oauth-client-starter): 添加登录信息模型注解
    - 在 LoginData 类上添加 Swagger 注解，用于生成 API 文档
    - 为每个字段添加 @ApiModelProperty 注解，描述字段含义
    - 添加 @ApiModel 注解，指定类的描述信息

--- spring-support-oauth-client-starter/src/main/java/com/chua/starter/oauth/client/support/provider/LoginData.java
+++ spring-support-oauth-client-starter/src/main/java/com/chua/starter/oauth/client/support/provider/LoginData.java
@@ -1,6 +1,8 @@
 package com.chua.starter.oauth.client.support.provider;
 
 import com.chua.starter.common.support.annotations.RequestParamMapping;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -11,15 +13,37 @@ import javax.validation.constraints.NotNull;
  * @author CH
  */
 @Data
+@ApiModel(description = "登录信息")
 public class LoginData {
 
+    /**
+     * 账号
+     */
+    @ApiModelProperty(value = "账号")
     @NotNull(message = "账号不能为空")
     private String username;
+
+    /**
+     * 密码
+     */
+    @ApiModelProperty(value = "密码(md5加密)")
     @NotNull(message = "密码不能为空")
     private String password;
+    /**
+     * 校验码
+     */
+    @ApiModelProperty(value = "校验码")
     @NotNull(message = "校验码不能为空")
     @RequestParamMapping(name = {"verifyCode", "verifyCodeKey"})
     private String verifyCodeKey;
+    /**
+     * 校验码
+     */
+    @ApiModelProperty(value = "校验码标识, 非必填")
     private String verifyCodeUlid;
+    /**
+     * 登录类型
+     */
+    @ApiModelProperty(value = "登录类型, 网页：WEB, 微信:WX")
     private String loginType;
 }
