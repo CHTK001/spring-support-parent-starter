a79b11ab17e8abdfaa2f2603344bb4544bfcd8ef
ACHTK <yemengzhidiandig@gmail.com>
2025-01-07 20:20:56 +0800

    refactor(spring-support-pay-starter): 重构支付模块商家订单相关代码
    - 优化了 PayMerchantOrderMapper 接口的代码结构，提高了代码可读性
    - 添加了 BaseResultMap 和 Base_Column_List 在 XML 文件中，为后续查询提供基础结构

--- spring-support-pay-starter/src/main/java/com/chua/starter/pay/support/mapper/PayMerchantOrderMapper.java
+++ spring-support-pay-starter/src/main/java/com/chua/starter/pay/support/mapper/PayMerchantOrderMapper.java
@@ -4,22 +4,17 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.chua.starter.pay.support.entity.PayMerchantOrder;
-import com.chua.starter.pay.support.pojo.PayMerchantOrderQueryRequest;
 import com.chua.starter.pay.support.pojo.PayOrderQueryRequest;
 import com.chua.starter.pay.support.pojo.PayOrderV1Request;
 import com.chua.starter.pay.support.pojo.PayReOrderQueryV1Request;
 import org.apache.ibatis.annotations.Param;
 
-/**
- * @author CH
- * @since 2024/12/30
- */
 public interface PayMerchantOrderMapper extends BaseMapper<PayMerchantOrder> {
     /**
      * 分页查询
      *
-     * @param page       分页
-     * @param request    请求
+     * @param page    分页
+     * @param request 请求
      * @return
      */
     IPage<PayMerchantOrder> pageForOrder(@Param("page") Page<PayMerchantOrder> page,
@@ -34,15 +29,15 @@ public interface PayMerchantOrderMapper extends BaseMapper<PayMerchantOrder> {
      * @return
      */
     IPage<PayMerchantOrder> order(@Param("page") Page<PayMerchantOrder> page,
-                                  @Param("request")PayOrderV1Request request,
-                                  @Param("sysUserId")Integer sysUserId);
+                                  @Param("request") PayOrderV1Request request,
+                                  @Param("sysUserId") Integer sysUserId);
 
     /**
      * 分页查询
      *
-     * @param page      分页
-     * @param request   查询条件
+     * @param page    分页
+     * @param request 查询条件
      * @return
      */
-    IPage<PayMerchantOrder> reOrder(@Param("page")Page<PayMerchantOrder> page, @Param("request")PayReOrderQueryV1Request request);
+    IPage<PayMerchantOrder> reOrder(@Param("page") Page<PayMerchantOrder> page, @Param("request") PayReOrderQueryV1Request request);
 }
