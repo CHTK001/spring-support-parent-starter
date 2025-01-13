132c09133080434ab14f648d1a28898e6108bd5a
ACHTK <yemengzhidiandig@gmail.com>
2025-01-13 13:09:15 +0800

    refactor(spring-support): 重构 MQTT 支付客户端
    - 更新 MqttProperties 中的字段名称- 重构 PayClientConfiguration，使用 MqttTemplate 替代 MicaClient
    - 重构 PayConfiguration，移除与 MQTT 无关的代码
    - 更新 PayListenerService，使用 MqttTemplate 替代 MicaSession
    - 删除未使用的 PayNotifyProperties 和 PayProperties 类
    - 更新项目版本号至 4.0.0.28

--- spring-support-pay-client-starter/src/main/java/com/chua/starter/pay/support/configuration/PayClientConfiguration.java
+++ spring-support-pay-client-starter/src/main/java/com/chua/starter/pay/support/configuration/PayClientConfiguration.java
@@ -1,24 +1,14 @@
 package com.chua.starter.pay.support.configuration;
 
-import com.chua.common.support.protocol.ClientSetting;
-import com.chua.common.support.utils.ClassUtils;
-import com.chua.mica.support.client.MicaClient;
-import com.chua.mica.support.client.session.MicaSession;
+import com.chua.starter.mqtt.support.template.MqttTemplate;
 import com.chua.starter.pay.support.annotations.OnPayListener;
-import com.chua.starter.pay.support.properties.PayNotifyProperties;
-import com.chua.starter.pay.support.properties.PayProperties;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.properties.bind.Binder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.util.StringUtils;
 
 /**
  * 客戶端配置
@@ -26,67 +16,23 @@ import org.springframework.util.StringUtils;
  * @author CH
  * @since 2025/1/3
  */
-@EnableConfigurationProperties({PayNotifyProperties.class, PayProperties.class})
-public class PayClientConfiguration implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
+public class PayClientConfiguration  {
 
     public static PayListenerService factory = new PayListenerService();
-    private PayNotifyProperties payNotifyProperties;
 
-    @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-        registry.registerBeanDefinition("paySmartInstantiationAwareBeanPostProcessor",
-                BeanDefinitionBuilder.genericBeanDefinition(PayListenerService.class, () -> factory)
-                        .getBeanDefinition());
-        if(!payNotifyProperties.isEnable()) {
-            return;
-        }
 
-        if(ClassUtils.isPresent("com.chua.starter.pay.support.configuration.PayConfiguration")) {
-            return;
-        }
 
-        if(payNotifyProperties.getType() == PayNotifyProperties.Type.MQTT) {
-            registerMqttClient(registry, payNotifyProperties);
-        }
-
+    @Bean
+    public PayListenerService payListenerService(MqttTemplate mqttTemplate) {
+        factory.register(mqttTemplate);
+        return factory;
     }
-
-    private void registerMqttClient(BeanDefinitionRegistry registry, PayNotifyProperties payNotifyProperties) {
-        PayNotifyProperties.MqttConfig mqttConfig = payNotifyProperties.getMqttConfig();
-
-        if(!StringUtils.hasLength(mqttConfig.getHost())) {
-            return;
-        }
-
-        MicaClient micaClient = new MicaClient(ClientSetting.builder()
-                .host(mqttConfig.getHost())
-                .port(mqttConfig.getPort())
-                .clientId(mqttConfig.getClientId())
-                .username(mqttConfig.getUsername())
-                .password(mqttConfig.getPassword())
-                .build());
-
-        micaClient.connect();
-        MicaSession session = (MicaSession) micaClient.createSession("default");
-        factory.register(session);
-
-        registry.registerBeanDefinition("MicaClient",
-                BeanDefinitionBuilder.genericBeanDefinition(MicaClient.class, () -> micaClient)
-                        .setDestroyMethodName("close")
-                        .getBeanDefinition());
-
-    }
-
 
     @Bean
     public PaySmartInstantiationAwareBeanPostProcessor getPaySmartInstantiationAwareBeanPostProcessor() {
         return new PaySmartInstantiationAwareBeanPostProcessor();
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.payNotifyProperties = Binder.get(applicationContext.getEnvironment()).bindOrCreate(PayNotifyProperties.PRE, PayNotifyProperties.class);
-    }
 
     public class PaySmartInstantiationAwareBeanPostProcessor implements SmartInstantiationAwareBeanPostProcessor {
         @Override
--- spring-support-pay-starter/src/main/java/com/chua/starter/pay/support/configuration/PayConfiguration.java
+++ spring-support-pay-starter/src/main/java/com/chua/starter/pay/support/configuration/PayConfiguration.java
@@ -1,42 +1,16 @@
 package com.chua.starter.pay.support.configuration;
 
-import com.chua.common.support.chain.ChainContext;
-import com.chua.common.support.chain.FilterChain;
-import com.chua.common.support.chain.filter.Filter;
-import com.chua.common.support.collection.Option;
-import com.chua.common.support.collection.Options;
-import com.chua.common.support.eventbus.EventRouter;
-import com.chua.common.support.eventbus.Eventbus;
-import com.chua.common.support.protocol.ClientSetting;
-import com.chua.common.support.protocol.ServerSetting;
-import com.chua.common.support.utils.*;
-import com.chua.mica.support.client.MicaClient;
-import com.chua.mica.support.client.session.MicaSession;
-import com.chua.mica.support.server.MicaServer;
-import com.chua.redis.support.eventbus.RedisEventbus;
+import com.chua.starter.mqtt.support.template.MqttTemplate;
 import com.chua.starter.pay.support.properties.PayMqttProperties;
-import com.chua.starter.pay.support.properties.PayNotifyProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.UUID;
 
 /**
  * 支付配置
@@ -54,107 +28,8 @@ import java.util.UUID;
 @EnableConfigurationProperties(PayMqttProperties.class)
 @EnableScheduling
 @EnableTransactionManagement
-public class PayConfiguration implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware, DisposableBean, CommandLineRunner {
+public class PayConfiguration {
 
 
-    private ServerProperties serverProperties;
-    private MicaServer micaServer;
-    private static MicaClient micaClient;
-    private PayNotifyProperties payNotifyProperties;
 
-    @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-        if (!payNotifyProperties.isEnable()) {
-            return;
-        }
-        try {
-            registerMqttServer();
-            registryMqttClient();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void registryMqttClient() {
-        PayNotifyProperties.MqttConfig mqttConfig = payNotifyProperties.getMqttConfig();
-        if(null == micaClient) {
-            micaClient = new MicaClient(
-                    ClientSetting.builder()
-                            .clientId(UUID.randomUUID().toString())
-                            .port(NumberUtils.isPositive(mqttConfig.getPort(), serverProperties.getPort() + 10000))
-                            .host(StringUtils.defaultString(mqttConfig.getHost(), "127.0.0.1"))
-                            .build()
-            );
-            Thread.ofVirtual()
-                    .name("延迟启动")
-                    .start(() -> {
-                        ThreadUtils.sleepSecondsQuietly(5);
-                        micaClient.connect();
-                        MicaSession session = (MicaSession) micaClient.createSession("default");
-                        PayClientConfiguration.factory.register(session);
-                    });
-        }
-
-
-    }
-
-    private void registerMqttServer() {
-        PayNotifyProperties.MqttConfig mqttConfig = payNotifyProperties.getMqttConfig();
-        if (!mqttConfig.isOpenServer()) {
-            return;
-        }
-        micaServer = new MicaServer(
-                ServerSetting.builder()
-                        .addDefaultMapping(false)
-                        .options(new Options().addOption("defaultPublish", new Option("false")))
-                        .port(NumberUtils.isPositive(mqttConfig.getPort(), serverProperties.getPort() + 10000))
-                        .host("0.0.0.0")
-                        .build()
-        );
-        try {
-            micaServer.start();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public void registerDataSource() {
-        //        Properties properties = new Properties();
-//        Map<String, DataSource> dataSourceMap = new HashMap<>(1);
-//        Collection<DataSource> beanList = SpringBeanUtils.getBeanList(DataSource.class);
-//        int index = 0;
-//        for (DataSource dataSource : beanList) {
-//            if (dataSource instanceof HikariDataSource hikariDataSource) {
-//                dataSourceMap.put("dataSource" + index ++,  hikariDataSource);
-//            }
-//        }
-//        List< LogicTable > logicTables = List.of(
-//                LogicTable.builder()
-//                        .logicTable("pay_merchant_order_water")
-//                        .strategy(Strategy.TABLE)
-//                        .logicColumnName("create_time")
-//                        .build()
-//        );
-//        V5ShardingTableFactory v5ShardingTableFactory = new V5ShardingTableFactory(properties, dataSourceMap, logicTables);
-//        DataSource dataSource = v5ShardingTableFactory.dataSource();
-//        System.out.println();
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        serverProperties = Binder.get(applicationContext.getEnvironment()).bindOrCreate("server", ServerProperties.class);
-        payNotifyProperties = Binder.get(applicationContext.getEnvironment()).bindOrCreate(PayNotifyProperties.PRE, PayNotifyProperties.class);
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        IoUtils.closeQuietly(micaServer);
-        IoUtils.closeQuietly(micaClient);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
