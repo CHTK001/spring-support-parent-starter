d57130e7bff21b4e37856a64d1f3bd0114a413bc
yemen <784412312@qq.com>
2025-02-14 16:22:07 +0800

    feat(spring-support): 优化数据权限处理逻辑
    - 重构数据权限处理逻辑，简化代码结构- 优化数据权限查询条件的生成
    - 更新版本号至4.0.0.30

--- spring-support-oauth-client-starter/src/main/java/com/chua/starter/oauth/client/support/oauth/OauthAuthService.java
+++ spring-support-oauth-client-starter/src/main/java/com/chua/starter/oauth/client/support/oauth/OauthAuthService.java
@@ -4,7 +4,10 @@ import com.chua.common.support.bean.BeanUtils;
 import com.chua.starter.common.support.oauth.AuthService;
 import com.chua.starter.common.support.oauth.CurrentUser;
 import com.chua.starter.common.support.utils.RequestUtils;
-import com.chua.starter.oauth.client.support.user.UserResult;
+import com.chua.starter.oauth.client.support.user.UserResume;
+import org.springframework.util.ConcurrentReferenceHashMap;
+
+import java.util.Map;
 
 /**
  * 权限服务
@@ -12,12 +15,14 @@ import com.chua.starter.oauth.client.support.user.UserResult;
  */
 public class OauthAuthService implements AuthService {
 
+    final static Map<UserResume, CurrentUser> CURRENT_USER = new ConcurrentReferenceHashMap<>(1024);
+
     @Override
     public CurrentUser getCurrentUser() {
-        UserResult userInfo = RequestUtils.getUserInfo(UserResult.class);
+        UserResume userInfo = RequestUtils.getUserInfo(UserResume.class);
         if(null == userInfo) {
             return null;
         }
-        return BeanUtils.copyProperties(userInfo, CurrentUser.class);
+        return CURRENT_USER.computeIfAbsent(userInfo, (key) -> BeanUtils.copyProperties(userInfo, CurrentUser.class));
     }
 }
