6143451a4ce4da7c86d6bd92a60d2d4259bca4a2
yemen <784412312@qq.com>
2025-03-05 13:54:33 +0800

    feat(system): 新增文生图模板功能并优化大模型接口- 新增文生图模板相关实体、Mapper、Service及其实现类 - 优化大模型接口，支持POST请求和异步任务 - 添加文件存储配置和相关组件 - 更新项目相关实体，增加临时文件路径和OSS地址字段 - 修复部分bug，如错误信息处理、模板查询等

---
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/configuration/FileStorageConfiguration.java
@@ -0,0 +1,99 @@
+package com.chua.starter.common.support.configuration;
+
+
+import com.chua.common.support.chain.filter.storage.FileStorageChainFilter;
+import com.chua.common.support.oss.FileStorage;
+import com.chua.common.support.oss.setting.BucketSetting;
+import com.chua.common.support.protocol.server.Server;
+import com.chua.common.support.utils.NumberUtils;
+import com.chua.common.support.utils.StringUtils;
+import com.chua.starter.common.support.filestorage.FileStorageProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * 文件存储
+ *
+ * @author CH
+ */
+@Slf4j
+@EnableConfigurationProperties(FileStorageProperties.class)
+public class FileStorageConfiguration implements BeanDefinitionRegistryPostProcessor, EnvironmentAware {
+    private FileStorageProperties fileStorageProperties;
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        if (!fileStorageProperties.isEnabled()) {
+            return;
+        }
+
+        List<FileStorageProperties.Setting> properties = fileStorageProperties.getProperties();
+        if (null == properties) {
+            return;
+        }
+        properties.forEach((value) -> {
+            registerFileStorage(registry, value);
+        });
+
+    }
+
+    /**
+     * 注册文件存储
+     *
+     * @param registry registry
+     * @param value    value
+     */
+    private void registerFileStorage(BeanDefinitionRegistry registry, FileStorageProperties.Setting value) {
+        FileStorage fileStorage = FileStorage.createStorage(value.getType(), BucketSetting.builder()
+                .endpoint(value.getEndpoint())
+                .bucket(value.getBucket())
+                .accessKeyId(value.getAccessKeyId())
+                .accessKeySecret(value.getAccessKeySecret())
+                .connectionTimeoutMills(value.getConnectionTimeoutMills())
+                .sessionTimeoutMills(value.getSessionTimeoutMills())
+                .region(value.getRegion())
+                .charset(value.getCharset())
+                .build());
+        String port = value.getPort();
+        String[] split = StringUtils.split(port, ",");
+        for (String s : split) {
+            s = s.trim();
+            if (!NumberUtils.isNumber(s)) {
+                continue;
+            }
+            var server = Server.create("http", Integer.parseInt(s));
+            server.addFilter(new FileStorageChainFilter(FileStorageChainFilter.FileStorageFactory.create(
+                            FileStorageChainFilter.FileStorageSetting.builder()
+                                    .webjars(true)
+                                    .build()
+
+                    )
+                    .addFileStorage("/" + value.getBucket(), fileStorage)));
+
+            registry.registerBeanDefinition("fileStorage" + UUID.randomUUID(), BeanDefinitionBuilder
+                    .rootBeanDefinition(Server.class, () -> server)
+                    .setInitMethodName("start")
+                    .setDestroyMethodName("stop")
+                    .getBeanDefinition()
+            );
+
+            log.info("文件存储服务启动成功,端口:{}", s);
+        }
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        fileStorageProperties = Binder.get(environment)
+                .bindOrCreate(FileStorageProperties.PRE, FileStorageProperties.class);
+    }
+}
