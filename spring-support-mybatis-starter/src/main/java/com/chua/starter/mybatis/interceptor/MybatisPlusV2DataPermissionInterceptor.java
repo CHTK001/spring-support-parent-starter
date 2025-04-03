05ce4e37899a9f6f95d37717505bf2b2c573e115
yemen <784412312@qq.com>
2025-04-03 09:06:36 +0800

    feat(school): 添加高考网学校数据同步功能
    - 新增 GaokaoSyncProvider 实现类，提供高考网学校数据同步功能- 添加学校信息和专业信息的解析和保存逻辑
    - 实现基本学校数据和专业数据的同步

--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/interceptor/MybatisPlusV2DataPermissionInterceptor.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/interceptor/MybatisPlusV2DataPermissionInterceptor.java
@@ -6,6 +6,7 @@ import com.chua.common.support.utils.ObjectUtils;
 import com.chua.starter.common.support.configuration.SpringBeanUtils;
 import com.chua.starter.common.support.oauth.AuthService;
 import com.chua.starter.common.support.oauth.CurrentUser;
+import com.chua.starter.mybatis.properties.MybatisPlusDataScopeProperties;
 import net.sf.jsqlparser.statement.delete.Delete;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.update.Update;
@@ -17,13 +18,25 @@ import net.sf.jsqlparser.statement.update.Update;
  */
 public class MybatisPlusV2DataPermissionInterceptor extends DataPermissionInterceptor {
 
-    public MybatisPlusV2DataPermissionInterceptor(DataPermissionHandler dataPermissionHandler) {
+    private final MybatisPlusDataScopeProperties methodSecurityInterceptor;
+
+    public MybatisPlusV2DataPermissionInterceptor(DataPermissionHandler dataPermissionHandler, MybatisPlusDataScopeProperties methodSecurityInterceptor) {
         super(dataPermissionHandler);
+        this.methodSecurityInterceptor = methodSecurityInterceptor;
     }
 
     @Override
     protected void processDelete(Delete delete, int index, String sql, Object obj) {
-        CurrentUser currentUser = SpringBeanUtils.getBean(AuthService.class).getCurrentUser();
+        if (!methodSecurityInterceptor.isEnable()) {
+            super.processDelete(delete, index, sql, obj);
+            return;
+        }
+
+        AuthService authService = SpringBeanUtils.getBean(AuthService.class);
+        if (null == authService) {
+            return;
+        }
+        CurrentUser currentUser = authService.getCurrentUser();
         if (ObjectUtils.isEmpty(currentUser)) {
             return;
         }
@@ -36,7 +49,15 @@ public class MybatisPlusV2DataPermissionInterceptor extends DataPermissionInterc
 
     @Override
     protected void processUpdate(Update update, int index, String sql, Object obj) {
-        CurrentUser currentUser = SpringBeanUtils.getBean(AuthService.class).getCurrentUser();
+        if (!methodSecurityInterceptor.isEnable()) {
+            super.processUpdate(update, index, sql, obj);
+            return;
+        }
+        AuthService authService = SpringBeanUtils.getBean(AuthService.class);
+        if (null == authService) {
+            return;
+        }
+        CurrentUser currentUser = authService.getCurrentUser();
         if (ObjectUtils.isEmpty(currentUser)) {
             return;
         }
@@ -49,7 +70,16 @@ public class MybatisPlusV2DataPermissionInterceptor extends DataPermissionInterc
 
     @Override
     protected void processSelect(Select select, int index, String sql, Object obj) {
-        CurrentUser currentUser = SpringBeanUtils.getBean(AuthService.class).getCurrentUser();
+        if (!methodSecurityInterceptor.isEnable()) {
+            super.processSelect(select, index, sql, obj);
+            return;
+        }
+
+        AuthService authService = SpringBeanUtils.getBean(AuthService.class);
+        if (null == authService) {
+            return;
+        }
+        CurrentUser currentUser = authService.getCurrentUser();
         if (ObjectUtils.isEmpty(currentUser)) {
             return;
         }
