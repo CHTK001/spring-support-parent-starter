ca1539e7d9362abeafa2ca215cc316ac21e5b60e
yemen <784412312@qq.com>
2025-06-27 16:15:41 +0800

    refactor(mybatis): 重构 Mybatis 拦截器和日志处理
    - 移除了 MybatisP6SpyLogger 中的线程局部变量和方法栈
    - 使用 MDC 传递 mapper 方法信息
    - 优化了 MapperTracingInterceptor 中的拦截逻辑
    - 重构了 MybatisP6SpyLogger 中的日志格式

--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/interceptor/MapperTracingInterceptor.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/interceptor/MapperTracingInterceptor.java
@@ -1,6 +1,5 @@
 package com.chua.starter.mybatis.interceptor;
 
-import com.chua.starter.mybatis.p6spy.MybatisP6SpyLogger;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.resultset.ResultSetHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
@@ -11,6 +10,7 @@ import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+import org.slf4j.MDC;
 
 import java.sql.Statement;
 
@@ -29,15 +29,15 @@ public class MapperTracingInterceptor implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        MappedStatement ms = (MappedStatement) invocation.getArgs()[0];
-        String mapperMethod = ms.getId();
-
-        MybatisP6SpyLogger.pushMethod(mapperMethod);
-        try {
-            return invocation.proceed();
-        } finally {
-            MybatisP6SpyLogger.popMethod();
+        Object arg = invocation.getArgs()[0];
+        if(arg instanceof MappedStatement ms) {
+            String mapperMethod = ms.getId();
+            try (MDC.MDCCloseable closeable = MDC.putCloseable("mapper", mapperMethod)) {
+                return invocation.proceed();
+            }
         }
+
+        return invocation.proceed();
     }
 
 
--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/p6spy/MybatisP6SpyLogger.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/p6spy/MybatisP6SpyLogger.java
@@ -2,9 +2,7 @@ package com.chua.starter.mybatis.p6spy;
 
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
-
-import java.util.ArrayDeque;
-import java.util.Deque;
+import org.slf4j.MDC;
 
 /**
  * mybatis p6spy日志
@@ -13,33 +11,13 @@ import java.util.Deque;
  * @since 2025/6/27 15:51
  */
 public class MybatisP6SpyLogger implements MessageFormattingStrategy {
-    private static final ThreadLocal<Deque<String>> methodStack = ThreadLocal.withInitial(ArrayDeque::new);
-
-    /**
-     * 压栈
-     *
-     * @param method 方法
-     */
-    public static void pushMethod(String method) {
-        methodStack.get().push(method);
-    }
-
-    /**
-     * 弹栈
-     */
-    public static void popMethod() {
-        Deque<String> stack = methodStack.get();
-        if (!stack.isEmpty()) {
-            stack.pop();
-        }
-    }
 
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         if (StringUtils.isNotBlank(sql)) {
-            String currentMethod = methodStack.get().peek();
-            if (null != currentMethod) {
-                return " 耗时: " + elapsed + " ms " + now + "\n 方法：" + currentMethod + " 执行的SQL：" + sql.replaceAll("[\\s]+", " ") + "\n";
+            String mapperMethod = MDC.get("mapper");
+            if (null != mapperMethod) {
+                return " 耗时: " + elapsed + " ms " + now + "\n 执行的方法 ：" + mapperMethod + " \n 执行的SQL ：" + sql.replaceAll("[\\s]+", " ") + "\n";
             }
             return " 耗时: " + elapsed + " ms " + now + "\n 执行的SQL：" + sql.replaceAll("[\\s]+", " ") + "\n";
         }
