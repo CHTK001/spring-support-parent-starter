5b67cd587692963d10f09e099f32f7fd6466584d
yemen <1@qq.com>
2025-08-16 15:58:21 +0800

    feat(spring-support-report-client-arthas-starter): 添加配置环境类
    - 新增 ReportDiscoveryEnvironment 类实现 DiscoveryEnvironment 接口
    - 用于配置报告客户端的 Arthas 端口
    - 提供 getName 和 getProperties 方法，返回环境名称和属性

---
+++ spring-support-report-client-arthas-starter/src/main/java/com/chua/report/client/arthas/starter/environment/ReportDiscoveryEnvironment.java
@@ -0,0 +1,31 @@
+package com.chua.report.client.starter.environment;
+
+import com.chua.report.client.starter.properties.ReportClientProperties;
+import com.chua.report.client.starter.setting.SettingFactory;
+import com.chua.starter.discovery.support.service.DiscoveryEnvironment;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Properties;
+
+/**
+ * 配置环境
+ * @author CH
+ */
+@RequiredArgsConstructor
+public class ReportDiscoveryEnvironment implements DiscoveryEnvironment {
+
+    final ArthasClientProperties arthasClientProperties;
+
+    public static final String REPORT_ARTHAS_CLIENT_PORT = "report.client.arthas.port";
+    @Override
+    public String getName() {
+        return "monitor";
+    }
+
+    @Override
+    public Properties getProperties() {
+        Properties properties = new Properties();
+        properties.put(REPORT_ARTHAS_CLIENT_PORT, arthasClientProperties.getTunnelAddress());
+        return properties;
+    }
+}
