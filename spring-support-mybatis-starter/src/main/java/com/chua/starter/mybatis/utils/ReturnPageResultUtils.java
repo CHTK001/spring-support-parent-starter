81f92be57c065f4538b0da9bbaabc389e195ca39
yemen <784412312@qq.com>
2025-09-20 09:55:14 +0800

    feat(jsoup): 移除冗余的自定义类并优化导入路径移除了 `AbstractChangeNotifyingArrayList` 和 `AbstractCombiningEvaluator` 等冗余实现类，统一使用标准库或更合理的抽象方式。同时将部分 Jsoup 相关类的导入路径从项目内部包调整为外部依赖库（如 org.jsoup），以减少重复代码，提升模块清晰度和可维护性。

--- spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/utils/ReturnPageResultUtils.java
+++ spring-support-mybatis-starter/src/main/java/com/chua/starter/mybatis/utils/ReturnPageResultUtils.java
@@ -2,7 +2,11 @@ package com.chua.starter.mybatis.utils;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.chua.common.support.lang.code.ReturnPageResult;
+import com.chua.common.support.utils.PageUtils;
 import com.chua.starter.mybatis.page.PagePreconditioning;
+
+import java.util.List;
 
 /**
  * 分页工具类
@@ -44,5 +48,31 @@ public class ReturnPageResultUtils extends PageResultUtils{
      */
     public static <T> PagePreconditioning<T> preconditioning(IPage<T> page) {
         return new PagePreconditioning<>(page);
+    }
+
+
+    /**
+     * 静态分页
+     *
+     * @param data 数据
+     * @param page 页码
+     * @param size 页大小
+     * @return 分页结果
+     */
+    public static <T> ReturnPageResult<T> staticPage(List<T> data, int page, int size) {
+        return PageResultUtils.ok(createPage(PageUtils.page(data, page, size), page, size));
+    }
+
+    /**
+     * 创建分页
+     *
+     * @param page1 数据
+     * @param page  页码
+     * @param size  页大小
+     * @param <T>   泛型
+     * @return 分页
+     */
+    private static <T> IPage<T> createPage(List<T> page1, int page, int size) {
+        return new Page<T>(page, size).setRecords(page1);
     }
 }
