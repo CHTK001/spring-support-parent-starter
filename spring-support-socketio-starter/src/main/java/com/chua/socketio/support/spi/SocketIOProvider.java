79308c83ac92196c7f8f45cd2e343e832ad4bcf1
yemen <784412312@qq.com>
2025-12-04 20:04:14 +0800

    docs: 更新 Git 提交信息规范并更新子项目引用
    - 将 git.md 触发器从 always_on 改为 manual
    - 重写 Git 提交信息规范，要求使用简体中文并按修改类型分组
    - 更新 spring-support-api-starter 子项目引用
    - 更新 spring-support-parent-starter 子项目引用
    - 更新 utils-support-parent-starter 子项目引用
    - 更新 vue-support-parent-starter 子项目引用

--- spring-support-socketio-starter/src/main/java/com/chua/socketio/support/spi/SocketIOProvider.java
+++ spring-support-socketio-starter/src/main/java/com/chua/socketio/support/spi/SocketIOProvider.java
@@ -1,6 +1,6 @@
 package com.chua.socketio.support.spi;
 
-import com.chua.common.support.spi.Spi;
+import com.chua.common.support.annotations.Spi;
 import com.chua.socket.support.SocketListener;
 import com.chua.socket.support.SocketProtocol;
 import com.chua.socket.support.properties.SocketProperties;
